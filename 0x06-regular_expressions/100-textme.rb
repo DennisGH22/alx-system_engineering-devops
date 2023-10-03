3ad52d5914ce8a4c12a47d7e1362fb2af74a8804
Tebogo Ndaba <dennistebogo@gmail.com>
2023-10-03 23:32:16 +0200

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 input_string = ARGV[0]
 regex = /\[from:(\+?\w+)\] \[to:(\+?\w+)\] \[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/
-puts input_string.scan(regex)
+puts input_string.scan(regex).join(",")
