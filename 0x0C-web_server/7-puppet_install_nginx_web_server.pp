5c8b1f12d51d015c24290730e158d0d64d522c74
Tebogo Ndaba <dennistebogo@gmail.com>
2023-11-29 00:27:29 +0200

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,5 @@
-# Ensure Nginx package is present
-package { 'nginx':
-  ensure => 'present',
-}
-
-# Install Nginx
-exec { 'install_nginx':
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx',
+# Install Nginx web server (w/ Puppet)
+exec { 'config_server':
   provider => shell,
-  require  => Package['nginx'],
-}
-
-# Create index.html with "Hello World!" content
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Exec['install_nginx'],
-}
-
-# Configure Nginx with additional location block
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "listen 80 default_server;\nlocation /redirect_me {\n\treturn 301 https://tebogondaba.tech/;\n}",
-  require => Exec['install_nginx'],
-}
-
-# Enable the additional site configuration
-exec { 'enable_site':
-  command  => 'sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/',
-  provider => shell,
-  require  => File['/etc/nginx/sites-available/default'],
-}
-
-# Restart Nginx
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => [File['/etc/nginx/sites-available/default'], Exec['enable_site']],
+  command  => 'sudo apt-get update; sudo apt-get -y install nginx; echo "Hello World!" | sudo tee /var/www/html/index.html; redirection_string="server_name _;\n\trewrite ^\/redirect_me https:\/\/tebogondaba.tech permanent;"; sudo sed -i "s/server_name _;/$redirection_string/" /etc/nginx/sites-available/default; sudo nginx -t; sudo service nginx restart',
 }
