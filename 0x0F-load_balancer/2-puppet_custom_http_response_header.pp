4478545353e9d2e680e475c654523e6ef161b810
Tebogo Ndaba <dennistebogo@gmail.com>
2023-12-05 03:29:09 +0200

    Add a custom HTTP header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# Add a custom HTTP header
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  provider => shell,
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default;
+  service nginx restart',
+}
