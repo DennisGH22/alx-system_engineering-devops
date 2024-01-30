a593ddeb313ad5a97e9e3b7ca50b0f8bc7329811
Tebogo Ndaba <dennistebogo@gmail.com>
2024-01-30 23:08:14 +0200

    Initial commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,6 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-
-    """
-    Queries the Reddit API and
-    returns the number of subscribers for a given subreddit.
-    """
-
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
     headers = {'User-Agent': 'my-reddit-client'}
