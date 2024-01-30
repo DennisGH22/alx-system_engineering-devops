871911865d2084d5a478c34571c865b27ab42847
Tebogo Ndaba <dennistebogo@gmail.com>
2024-01-30 23:57:51 +0200

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Print titles of the first 10 hot posts for a subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    base_url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-agent': 'my-reddit-client'}
+
+    try:
+        response = requests.get(base_url, headers=headers)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
