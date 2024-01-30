7b4e4a555e78d13b08d0608acebaab1faafb05bd
Tebogo Ndaba <dennistebogo@gmail.com>
2024-01-30 23:31:46 +0200

    Initial commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = 'https://www.reddit.com/r/{}/about.json'
 
     headers = {'User-Agent': 'my-reddit-client'}
 
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url.format(subreddit), headers=headers)
 
         if response.status_code == 200:
             data = response.json()
