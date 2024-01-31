b9382b2caabe8c47d1abebf2249cf1ba4a26f539
Tebogo Ndaba <dennistebogo@gmail.com>
2024-01-31 12:23:10 +0200

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Print titles of the first 10 hot posts for a subreddit."""
-import requests
 
 
 def top_ten(subreddit):
+    import requests
+
     base_url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
 
